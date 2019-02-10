d90315ace60752995672aab9e87000b036eb8327
Joel Samson <sams0050@algonquinlive.com>
2019-02-09 21:26:13 -0500

    finished assignment1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,49 +1,60 @@
 <template>
-  <el-form ref="form" :model="form" label-width="120px">
-    <el-form-item label="First Name">
-      <el-input v-model="form.name"></el-input>
-    </el-form-item>
-    <el-form-item label="Last Name">
-      <el-input v-model="form.name2"></el-input>
-    </el-form-item>
-    <el-form-item
-      prop="email"
-      label="Email"
-      :rules="[
+  <div class="forms">
+    <el-form ref="form" :model="form" label-width="20px">
+      <el-form-item label="First Name">
+        <el-input v-model="form.name"></el-input>
+      </el-form-item>
+      <el-form-item label="Last Name">
+        <el-input v-model="form.name2"></el-input>
+      </el-form-item>
+      <el-form-item
+        prop="email"
+        label="Email"
+        :rules="[
       { required: true, message: 'Please input email address', trigger: 'blur' },
       { type: 'email', message: 'Please input correct email address', trigger: ['blur', 'change'] }
     ]"
-    >
-      <el-input v-model="form.email"></el-input>
-    </el-form-item>
-    <el-form-item label="Subject">
-      <el-input v-model="form.subject"></el-input>
-    </el-form-item>
-    <el-form-item label="Request Form Info">
-      <el-input
-        type="textarea"
-        autosize="{ minRows: 2, maxRows:"
-        placeholder="Type Here"
-        v-model="textarea2"
-      ></el-input>
-    </el-form-item>
-    <div style="margin: 20px 0;"></div>
-    <!-- `checked` should be true or false -->
-    <el-checkbox v-model="checked">I Agree</el-checkbox>
-    <el-form-item>
-      <el-button type="primary" @click="onSubmit">Create</el-button>
-      <el-button>Cancel</el-button>
-    </el-form-item>
-    <el-button type="text" @click="centerDialogVisible = true">Submit</el-button>
+      >
+        <el-input v-model="form.email"></el-input>
+      </el-form-item>
+      <el-form-item label="Subject">
+        <el-input v-model="form.subject"></el-input>
+      </el-form-item>
+      <el-form-item label="Request Form Info">
+        <el-input
+          type="textarea"
+          autosize="{ minRows: 2, maxRows:"
+          placeholder="Type Here"
+          v-model="textarea2"
+        ></el-input>
+      </el-form-item>
+      <div style="margin: 20px 0;"></div>
+      <!-- `checked` should be true or false -->
+      <el-form-item label="Terms & Conditions">
+        <el-checkbox v-model="unchecked">I Agree</el-checkbox>
+      </el-form-item>
 
-    <el-dialog title="Warning" :visible.sync="centerDialogVisible" width="30%" center>
-      <span>It should be noted that the content will not be aligned in center by default</span>
-      <span slot="footer" class="dialog-footer">
-        <el-button @click="centerDialogVisible = false">Cancel</el-button>
-        <el-button type="primary" @click="centerDialogVisible = false">Confirm</el-button>
-      </span>
-    </el-dialog>
-  </el-form>
+      <el-form-item>
+        <el-button type="primary" @click="centerDialogVisible = true">Submit</el-button>
+      </el-form-item>
+
+      <el-dialog
+        class="popup"
+        :visible.sync="centerDialogVisible"
+        width="100%"
+        fullscreen="true"
+        modal-append-to-body="true"
+        center
+      >
+        <div style="margin: 25px;"></div>
+        <span>Confirm to Submit</span>
+        <span slot="footer" class="dialog-footer">
+          <el-button type="text" @click="centerDialogVisible = false">Confirm</el-button>
+          <el-button @click="centerDialogVisible = false">Cancel</el-button>
+        </span>
+      </el-dialog>
+    </el-form>
+  </div>
 </template>
 
 <script>
